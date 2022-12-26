109192580df62961b9a2b3c97c5a16170f82a48a
anzaniy <83295231+anzaniy@users.noreply.github.com>
2022-12-26 14:10:55 +0700

    update branch

--- application/views/depan/v_home.php
+++ application/views/depan/v_home.php
@@ -6,21 +6,21 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>SELAMAT DATANG DI WEBSITE SMA AL KAUTSAR BANDAR LAMPUNG</title>
-    <link rel="shorcut icon" href="<?php echo base_url() . 'theme/images/icon.png' ?>">
+    <link rel="shorcut icon" href="theme/images/icon.png">
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="<?php echo base_url() . 'theme/css/bootstrap.min.css' ?>">
+    <link rel="stylesheet" href="theme/css/bootstrap.min.css">
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Lora:400,700" rel="stylesheet">
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="<?php echo base_url() . 'theme/css/font-awesome.min.css' ?>">
+    <link rel="stylesheet" href="theme/css/font-awesome.min.css">
     <!-- Simple Line Font -->
-    <link rel="stylesheet" href="<?php echo base_url() . 'theme/css/simple-line-icons.css' ?>">
+    <link rel="stylesheet" href="theme/css/simple-line-icons.css">
     <!-- Slider / Carousel -->
-    <link rel="stylesheet" href="<?php echo base_url() . 'theme/css/slick.css' ?>">
-    <link rel="stylesheet" href="<?php echo base_url() . 'theme/css/slick-theme.css' ?>">
-    <link rel="stylesheet" href="<?php echo base_url() . 'theme/css/owl.carousel.min.css' ?>">
+    <link rel="stylesheet" href="theme/css/slick.css">
+    <link rel="stylesheet" href="theme/css/slick-theme.css">
+    <link rel="stylesheet" href="theme/css/owl.carousel.min.css">
     <!-- Main CSS -->
-    <link href="<?php echo base_url() . 'theme/css/style.css' ?>" rel="stylesheet">
+    <link href="theme/css/style.css" rel="stylesheet">
     <?php
     function limit_words($string, $word_limit)
     {
@@ -73,7 +73,7 @@
                             <span class="icon-menu"></span>
                         </button>
                         
-                        <a href="<?php echo site_url(''); ?>" class="navbar-brand nav-brand2"><img class="img img-responsive" width="200px;" src="<?php echo base_url() . 'theme/images/logo-dark.png' ?>"></a>
+                        <a href="<?php echo site_url(''); ?>" class="navbar-brand nav-brand2"><img class="img img-responsive" width="200px;" src="theme/images/logo-dark.png"></a>
                         
                         <!-- copy ke semua menu-->
                         <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
@@ -156,7 +156,7 @@
                 </ol>
                 <div class="carousel-inner" role="listbox">
                     <div class="carousel-item active">
-                        <img class="d-block" src="<?php echo base_url() . 'theme/images/slider.jpg' ?>" alt="First slide">
+                        <img class="d-block" src="theme/images/slider.jpg" alt="First slide">
                         <div class="carousel-caption d-md-block">
                             <div class="slider_title">
                                 <h1>
@@ -172,7 +172,7 @@
                         </div>
                     </div>
                     <div class="carousel-item">
-                        <img class="d-block" src="<?php echo base_url() . 'theme/images/slider-2.jpg' ?>" alt="Second slide">
+                        <img class="d-block" src="theme/images/slider-2.jpg" alt="Second slide">
                         <div class="carousel-caption d-md-block">
                             <div class="slider_title">
                                 <h1>Guru Bekualitas Tinggi</h1>
@@ -184,7 +184,7 @@
                         </div>
                     </div>
                     <div class="carousel-item">
-                        <img class="d-block" src="<?php echo base_url() . 'theme/images/slider-3.jpg' ?>" alt="Third slide">
+                        <img class="d-block" src="theme/images/slider-3.jpg" alt="Third slide">
                         <div class="carousel-caption d-md-block">
                             <div class="slider_title">
                                 <h1>Proses Belajar Interatif</h1>
@@ -218,7 +218,7 @@
 
                 </div>
                 <div class="col-md-4">
-                    <img src="<?php echo base_url() . 'theme/images/welcome.png' ?>" class="img-fluid about-img" alt="#"> <!-- !-->
+                    <img src="theme/images/welcome.png" class="img-fluid about-img" alt="#"> <!-- !-->
                 </div>
             </div>
         </div>
@@ -237,7 +237,7 @@
                     <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3">
                         <div class="courses_box mb-4">
                             <div class="course-img-wrap">
-                                <img src="<?php echo base_url() . 'assets/images/' . $row->tulisan_gambar; ?>" class="img-fluid" alt="courses-img">
+                                <img src="assets/images/' . $row->tulisan_gambar; ?>" class="img-fluid" alt="courses-img">
                             </div>
                             <!-- // end .course-img-wrap -->
                             <a href="<?php echo site_url('artikel/' . $row->tulisan_slug); ?>" class="course-box-content">
@@ -263,7 +263,7 @@
                     <div class="event-img2">
                         <?php foreach ($pengumuman->result() as $row) : ?>
                             <div class="row">
-                                <div class="col-sm-3"> <img src="<?php echo base_url() . 'theme/images/announcement-icon.png' ?>" class="img-fluid" alt="event-img"></div><!-- // end .col-sm-3 -->
+                                <div class="col-sm-3"> <img src="theme/images/announcement-icon.png" class="img-fluid" alt="event-img"></div><!-- // end .col-sm-3 -->
                                 <div class="col-sm-9">
                                     <h3><a href="<?php echo site_url('pengumuman'); ?>"><?php echo $row->pengumuman_judul; ?></a></h3>
                                     <span><?php echo $row->tanggal; ?></span>
@@ -305,7 +305,7 @@
             <div class="row">
                 <div class="col-sm-3">
                     <div class="chart-img">
-                        <img src="<?php echo base_url() . 'theme/images/chart-icon_1.png' ?>" class="img-fluid" alt="chart_icon">
+                        <img src="theme/images/chart-icon_1.png" class="img-fluid" alt="chart_icon">
                     </div>
                     <div class="chart-text">
                         <p><span class="counter"><?php echo $tot_guru; ?></span> Guru
@@ -314,7 +314,7 @@
                 </div>
                 <div class="col-sm-2">
                     <div class="chart-img">
-                        <img src="<?php echo base_url() . 'theme/images/chart-icon_2.png' ?>" class="img-fluid" alt="chart_icon">
+                        <img src="theme/images/chart-icon_2.png" class="img-fluid" alt="chart_icon">
                     </div>
                     <div class="chart-text">
                         <p><span class="counter"><?php echo $tot_siswa; ?></span> Siswa
@@ -323,7 +323,7 @@
                 </div>
                 <div class="col-sm-2">
                     <div class="chart-img">
-                        <img src="<?php echo base_url() . 'theme/images/chart-icon_3.png' ?>" class="img-fluid" alt="chart_icon">
+                        <img src="theme/images/chart-icon_3.png" class="img-fluid" alt="chart_icon">
                     </div>
                     <div class="chart-text">
                         <p><span class="counter"><?php echo $tot_files; ?></span> Download
@@ -332,7 +332,7 @@
                 </div>
                 <div class="col-sm-3">
                     <div class="chart-img">
-                        <img src="<?php echo base_url() . 'theme/images/chart-icon_4.png' ?>" class="img-fluid" alt="chart_icon">
+                        <img src="theme/images/chart-icon_4.png" class="img-fluid" alt="chart_icon">
                     </div>
                     <div class="chart-text">
                         <p><span class="counter"><?php echo $tot_agenda; ?></span> Agenda</p>
@@ -340,7 +340,7 @@
                 </div>
                 <div class="col-sm-2">
                     <div class="chart-img">
-                        <img src="<?php echo base_url() . 'theme/images/chart-icon_5.png' ?>" class="img-fluid" alt="chart_icon">
+                        <img src="theme/images/chart-icon_5.png" class="img-fluid" alt="chart_icon">
                     </div>
                     <div class="chart-text">
                         <?php
@@ -372,7 +372,7 @@
                 <div class="col-md-3">
                     <div class="foot-logo">
                         <a href="<?php echo site_url(); ?>">
-                            <img src="<?php echo base_url() . 'theme/images/logo-white.png' ?>" class="img-fluid" alt="footer_logo">
+                            <img src="theme/images/logo-white.png" class="img-fluid" alt="footer_logo">
                         </a>
                         <p><?php echo date('Y'); ?> © copyright by <a href="" target="_blank">M Fikri</a>. <br>All rights reserved.</p>
                     </div>
@@ -421,20 +421,20 @@
     </footer>
     <!--//END FOOTER -->
     <!-- jQuery, Bootstrap JS. -->
-    <script src="<?php echo base_url() . 'theme/js/jquery.min.js' ?>"></script>
-    <script src="<?php echo base_url() . 'theme/js/tether.min.js' ?>"></script>
-    <script src="<?php echo base_url() . 'theme/js/bootstrap.min.js' ?>"></script>
+    <script src="theme/js/jquery.min.js"></script>
+    <script src="theme/js/tether.min.js"></script>
+    <script src="theme/js/bootstrap.min.js"></script>
     <!-- Plugins -->
-    <script src="<?php echo base_url() . 'theme/js/slick.min.js' ?>"></script>
-    <script src="<?php echo base_url() . 'theme/js/waypoints.min.js' ?>"></script>
-    <script src="<?php echo base_url() . 'theme/js/counterup.min.js' ?>"></script>
-    <script src="<?php echo base_url() . 'theme/js/owl.carousel.min.js' ?>"></script>
-    <script src="<?php echo base_url() . 'theme/js/validate.js' ?>"></script>
-    <script src="<?php echo base_url() . 'theme/js/tweetie.min.js' ?>"></script>
+    <script src="theme/js/slick.min.js"></script>
+    <script src="theme/js/waypoints.min.js"></script>
+    <script src="theme/js/counterup.min.js"></script>
+    <script src="theme/js/owl.carousel.min.js"></script>
+    <script src="theme/js/validate.js"></script>
+    <script src="theme/js/tweetie.min.js"></script>
     <!-- Subscribe -->
-    <script src="<?php echo base_url() . 'theme/js/subscribe.js' ?>"></script>
+    <script src="theme/js/subscribe.js"></script>
     <!-- Script JS -->
-    <script src="<?php echo base_url() . 'theme/js/script.js' ?>"></script>
+    <script src="theme/js/script.js"></script>
     <!-- Script JS Bootstrap 5 gaes-->
     <!--<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>-->
     

4cc4c53949586e808b0bd2f54ee477d658efc502
anzaniy <83295231+anzaniy@users.noreply.github.com>
2022-12-29 08:09:05 +0700

    Update v_home.php

--- application/views/depan/v_home.php
+++ application/views/depan/v_home.php
@@ -36,6 +36,10 @@
 </head>
 
 <body>
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
     <section>
         <div class="slider_img layout_two">
             <div id="carousel" class="carousel slide" data-ride="carousel">

c63569490c6442877bfd7a3d0266005fcb805bbf
anzaniy <83295231+anzaniy@users.noreply.github.com>
2023-01-04 11:11:56 +0700

    a

--- application/views/depan/v_download.php
+++ application/views/depan/v_download.php
@@ -6,133 +6,43 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Download</title>
-    <link rel="shorcut icon" href="<?php echo base_url() . 'theme/images/icon.png' ?>">
+    <link href="assets/aset/img/icon.png" rel="icon">
+    <link href="assets/aset/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <link rel="shorcut icon" href="theme/images/icon.png">
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="<?php echo base_url() . 'theme/css/bootstrap.min.css' ?>">
+    <link rel="stylesheet" href="theme/css/bootstrap.min.css">
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Lora:400,700" rel="stylesheet">
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="<?php echo base_url() . 'theme/css/font-awesome.min.css' ?>">
+    <link rel="stylesheet" href="theme/css/font-awesome.min.css">
     <!-- Simple Line Font -->
-    <link rel="stylesheet" href="<?php echo base_url() . 'theme/css/simple-line-icons.css' ?>">
-    <!-- Owl Carousel -->
-    <link rel="stylesheet" href="<?php echo base_url() . 'theme/css/owl.carousel.min.css' ?>">
+    <link rel="stylesheet" href="theme/css/simple-line-icons.css">
+    <!-- Slider / Carousel -->
+    <link rel="stylesheet" href="theme/css/slick.css">
+    <link rel="stylesheet" href="theme/css/slick-theme.css">
+    <link rel="stylesheet" href="theme/css/owl.carousel.min.css">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="assets/aset/vendor/animate.css/animate.min.css" rel="stylesheet">
+    <link href="assets/aset/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/aset/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/aset/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/aset/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/aset/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="assets/aset/css/style.css" rel="stylesheet">
     <!-- Main CSS -->
-    <link href="<?php echo base_url() . 'theme/css/style.css' ?>" rel="stylesheet">
-    <link href="<?php echo base_url() . 'theme/css/dataTables.bootstrap4.min.css' ?>" rel="stylesheet">
+    <link href="theme/css/style.css" rel="stylesheet">
 
 </head>
 
 <body>
-    <!--============================= HEADER =============================-->
-    <div class="header-topbar">
-        <div class="container">
-            <div class="row">
-                <div class="col-xs-6 col-sm-8 col-md-9">
-                    <div class="header-top_address">
-                        <div class="header-top_list">
-                            <span class="icon-phone"></span>0721781578
-                        </div>
-                        <div class="header-top_list">
-                            <span class="icon-envelope-open"></span>smaalkautsarlampung@gmail.com
-                        </div>
-                        <div class="header-top_list">
-                            <span class="icon-location-pin"></span>Bandar Lampung, Lampung, INA. 35144
-                        </div>
-                    </div>
-                    <div class="header-top_login2">
-                        <a href="<?php echo site_url('contact'); ?>">Hubungi Kami</a>
-                    </div>
-                </div>
-                <div class="col-xs-6 col-sm-4 col-md-3">
-                    <div class="header-top_login mr-sm-3">
-                        <a href="<?php echo site_url('contact'); ?>">Hubungi Kami</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div data-toggle="affix" style="border-bottom:solid 1px #f2f2f2;">
-        <div class="container nav-menu2">
-            <div class="row">
-                <div class="col-md-12">
-                    <nav class="navbar navbar2 navbar-toggleable-md navbar-light bg-faded">
-                        <button class="navbar-toggler navbar-toggler2 navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavDropdown">
-                            <span class="icon-menu"></span>
-                        </button>
-                        <a href="<?php echo site_url(''); ?>" class="navbar-brand nav-brand2"><img class="img img-responsive" width="200px;" src="<?php echo base_url() . 'theme/images/logo-dark.png' ?>"></a>
-                        
-                        <!-- copy ke semua menu-->
-                        <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
-                            
-                            <ul class="navbar-nav">
-                                <li class="nav-item">
-                                    <a class="nav-link" href="<?php echo site_url(''); ?>">Home</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="<?php echo site_url('about'); ?>">About</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" target="blank" href="http://sis.smas-alkautsar.sch.id/">SIS</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="<?php echo site_url('visi_misi'); ?>">Visi_Misi</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="<?php echo site_url('struktur'); ?>">Struktur</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="<?php echo site_url('guru'); ?>">Guru</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="<?php echo site_url('siswa'); ?>">Siswa</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="<?php echo site_url('blog'); ?>">Blog</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="<?php echo site_url('pengumuman'); ?>">Pengumuman</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="<?php echo site_url('agenda'); ?>">Agenda</a>
-                                </li>
-                            <!--    <li class="nav-item">-->
-                            <!--        <a class="nav-link" href="<?php echo site_url('download'); ?>">Download</a>-->
-                            <!--    </li>-->
-                            <!--    <li class="nav-item">-->
-                            <!--        <a class="nav-link" href="<?php echo site_url('galeri'); ?>">Gallery</a>-->
-                            <!--    </li>-->
-                            <!--    <li class="nav-item">-->
-                            <!--        <a class="nav-link" href="<?php echo site_url('contact'); ?>">Contact</a>-->
-                            <!--    </li>-->
-                            </ul>
-                            
-                            
-                            <!-- drop down-->
-                            
-                            <div class="dropdown">
-                                  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    Menu
-                                  </button>
-                                  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                    <a class="dropdown-item" href="<?php echo site_url('download'); ?>">Download</a>
-                                    <a class="dropdown-item" href="<?php echo site_url('galeri'); ?>">Gallery</a>
-                                    <a class="dropdown-item" href="<?php echo site_url('contact'); ?>">Contact</a>
-                                  </div>
-                            </div>
-                            <!-- end drop down-->
-                            
-                        </div>
-                        <!-- end copy untuk kesemua menu-->
-                        
-                    </nav>
-                </div>
-            </div>
-        </div>
-    </div>
-    <section>
-    </section>
-    <!--//END HEADER -->
+    
     <section class="contact" style="margin-bottom:50px;">
         <div class="container">
             <div class="row">
@@ -177,59 +87,7 @@
     </section>
     <!--//END  ABOUT IMAGE -->
 
-    <!--============================= FOOTER =============================-->
-    <footer>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-3">
-                    <div class="foot-logo">
-                        <a href="<?php echo site_url(); ?>">
-                            <img src="<?php echo base_url() . 'theme/images/logo-white.png' ?>" class="img-fluid" alt="footer_logo">
-                        </a>
-                        <p><?php echo date('Y'); ?> © copyright by <a href="http://mfikri.com" target="_blank">M Fikri</a>. <br>All rights reserved.</p>
-                    </div>
-                </div>
-                <div class="col-md-3">
-                    <div class="sitemap">
-                        <h3>Menu Utama</h3>
-                        <ul>
-                            <li><a href="<?php echo site_url(); ?>">Home</a></li>
-                            <li><a href="<?php echo site_url('about'); ?>">About</a></li>
-                            <li><a href="<?php echo site_url('artikel'); ?>">Blog </a></li>
-                            <li><a href="<?php echo site_url('galeri'); ?>">Gallery</a></li>
-                            <li><a href="<?php echo site_url('contact'); ?>">Contact</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-md-3">
-                    <div class="sitemap">
-                        <h3>Akademik</h3>
-                        <ul>
-                            <li><a href="<?php echo site_url('guru'); ?>">Guru</a></li>
-                            <li><a href="<?php echo site_url('siswa'); ?>">Siswa </a></li>
-                            <li><a href="<?php echo site_url('pengumuman'); ?>">Pengumuman</a></li>
-                            <li><a href="<?php echo site_url('agenda'); ?>">Agenda</a></li>
-                            <li><a href="<?php echo site_url('download'); ?>">Download</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-md-3">
-                    <div class="address">
-                        <h3>Hubungi Kami</h3>
-                        <p><span>Alamat: </span> BANDAR LAMPUNG, LAMPUNG, INA. 35144</p>
-                        <p>Email : smaalkautsarlampung@gmail.com</p>
-                        <br> Phone : 0721 781578
-                        <ul class="footer-social-icons">
-                            <li><a href="#"><i class="fa fa-facebook fa-fb" aria-hidden="true"></i></a></li>
-                            <li><a href="#"><i class="fa fa-linkedin fa-in" aria-hidden="true"></i></a></li>
-                            <li><a href="#"><i class="fa fa-twitter fa-tw" aria-hidden="true"></i></a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </footer>
-    <!--//END FOOTER -->
+   
     <!-- jQuery, Bootstrap JS. -->
     <script src="<?php echo base_url() . 'theme/js/jquery.min.js' ?>"></script>
     <script src="<?php echo base_url() . 'theme/js/tether.min.js' ?>"></script>

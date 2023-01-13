f7c39a9b453181a1b528ebd0e92bbb86047e272b
anzaniy <83295231+anzaniy@users.noreply.github.com>
2023-01-13 07:55:39 +0700

    Update Prestasi.php

--- application/controllers/Prestasi.php
+++ application/controllers/Prestasi.php
@@ -1,130 +1,21 @@
 <?php
-class Guru extends CI_Controller{
-	function __construct(){
-		parent::__construct();
-		if($this->session->userdata('masuk') !=TRUE){
-            $url=base_url('administrator');
-            redirect($url);
-        };
-		$this->load->model('m_prestasi');
-		$this->load->model('m_pengguna');
-		$this->load->library('upload');
-	}
-
-
-	function index(){
-		$x['data']=$this->m_guru->get_all_guru();
-		$this->load->view('admin/v_prestasi',$x);
-	}
-	
-	function simpan_guru(){
-
-	            $this->upload->initialize($config);
-	            if(!empty($_FILES['filefoto']['name']))
-	            {
-	                if ($this->upload->do_upload('filefoto'))
-	                {
-	                        $gbr = $this->upload->data();
-	                        //Compress Image
-	                        $config['image_library']='gd2';
-	                        $config['source_image']='./assets/images/'.$gbr['file_name'];
-	                        $config['create_thumb']= FALSE;
-	                        $config['maintain_ratio']= FALSE;
-	                        $config['quality']= '60%';
-	                        $config['width']= 300;
-	                        $config['height']= 300;
-	                        $config['new_image']= './assets/images/'.$gbr['file_name'];
-	                        $this->load->library('image_lib', $config);
-	                        $this->image_lib->resize();
-
-	                        $photo=$gbr['file_name'];
-							$nip=strip_tags($this->input->post('xnip'));
-							$nama=strip_tags($this->input->post('xnama'));
-							$jenkel=strip_tags($this->input->post('xjenkel'));
-							$tmp_lahir=strip_tags($this->input->post('xtmp_lahir'));
-							$tgl_lahir=strip_tags($this->input->post('xtgl_lahir'));
-							$mapel=strip_tags($this->input->post('xmapel'));
-
-							$this->m_guru->simpan_guru($nip,$nama,$jenkel,$tmp_lahir,$tgl_lahir,$mapel,$photo);
-							echo $this->session->set_flashdata('msg','success');
-							redirect('admin/guru');
-					}else{
-	                    echo $this->session->set_flashdata('msg','warning');
-	                    redirect('admin/guru');
-	                }
-	                 
-	            }
-				
-	}
-	
-	function update_guru(){
-				
-	            $config['upload_path'] = './assets/images/'; //path folder
-	            $config['allowed_types'] = 'gif|jpg|png|jpeg|bmp'; //type yang dapat diakses bisa anda sesuaikan
-	            $config['encrypt_name'] = TRUE; //nama yang terupload nantinya
-
-	            $this->upload->initialize($config);
-	            if(!empty($_FILES['filefoto']['name']))
-	            {
-	                if ($this->upload->do_upload('filefoto'))
-	                {
-	                        $gbr = $this->upload->data();
-	                        //Compress Image
-	                        $config['image_library']='gd2';
-	                        $config['source_image']='./assets/images/'.$gbr['file_name'];
-	                        $config['create_thumb']= FALSE;
-	                        $config['maintain_ratio']= FALSE;
-	                        $config['quality']= '60%';
-	                        $config['width']= 300;
-	                        $config['height']= 300;
-	                        $config['new_image']= './assets/images/'.$gbr['file_name'];
-	                        $this->load->library('image_lib', $config);
-	                        $this->image_lib->resize();
-	                        $gambar=$this->input->post('gambar');
-							$path='./assets/images/'.$gambar;
-							unlink($path);
-
-	                        $photo=$gbr['file_name'];
-	                        $kode=$this->input->post('kode');
-							$nip=strip_tags($this->input->post('xnip'));
-							$nama=strip_tags($this->input->post('xnama'));
-							$jenkel=strip_tags($this->input->post('xjenkel'));
-							$tmp_lahir=strip_tags($this->input->post('xtmp_lahir'));
-							$tgl_lahir=strip_tags($this->input->post('xtgl_lahir'));
-							$mapel=strip_tags($this->input->post('xmapel'));
-
-							$this->m_guru->update_guru($kode,$nip,$nama,$jenkel,$tmp_lahir,$tgl_lahir,$mapel,$photo);
-							echo $this->session->set_flashdata('msg','info');
-							redirect('admin/guru');
-	                    
-	                }else{
-	                    echo $this->session->set_flashdata('msg','warning');
-	                    redirect('admin/guru');
-	                }
-	                
-	            }else{
-							$kode=$this->input->post('kode');
-							$nip=strip_tags($this->input->post('xnip'));
-							$nama=strip_tags($this->input->post('xnama'));
-							$jenkel=strip_tags($this->input->post('xjenkel'));
-							$tmp_lahir=strip_tags($this->input->post('xtmp_lahir'));
-							$tgl_lahir=strip_tags($this->input->post('xtgl_lahir'));
-							$mapel=strip_tags($this->input->post('xmapel'));
-							$this->m_guru->update_guru_tanpa_img($kode,$nip,$nama,$jenkel,$tmp_lahir,$tgl_lahir,$mapel);
-							echo $this->session->set_flashdata('msg','info');
-							redirect('admin/guru');
-	            } 
-
-	}
-
-	function hapus_guru(){
-		$kode=$this->input->post('kode');
-		$gambar=$this->input->post('gambar');
-		$path='./assets/images/'.$gambar;
-		unlink($path);
-		$this->m_guru->hapus_guru($kode);
-		echo $this->session->set_flashdata('msg','success-hapus');
-		redirect('admin/guru');
-	}
-
+class prestasi extends CI_Controller
+{
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->view('templates/header_1');
+        $this->load->model('m_pengunjung');
+        $this->m_pengunjung->count_visitor();
+    }
+    function index()
+    {
+        $x['tot_guru'] = $this->db->get('tbl_guru')->num_rows();
+        $x['tot_siswa'] = $this->db->get('tbl_siswa')->num_rows();
+        $x['tot_files'] = $this->db->get('tbl_files')->num_rows();
+        $x['tot_agenda'] = $this->db->get('tbl_agenda')->num_rows();
+        $this->load->view('depan/v_prestasi', $x);
+        $this->load->view('templates/counter');
+        $this->load->view('templates/footer');
+    }
 }

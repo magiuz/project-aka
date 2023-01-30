e7da155cb046cd3cc4cef1a0d08f1bac498d0504
Ages Mahesa <agesmahesa1134@gmail.com>
2023-01-30 13:39:59 +0700

    typo

--- application/views/depan/v_sejarah.php
+++ application/views/depan/v_sejarah.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Sejarah</title>
-    
+
     <link href="assets/aset/img/favicon.png" rel="icon">
     <link href="assets/aset/img/apple-touch-icon.png" rel="apple-touch-icon">
     <link rel="shorcut icon" href="theme/images/icon.png">
@@ -50,8 +50,8 @@
 
 <body>
     <style>
-        .container{
-            font-family:"poppins";
+        .container {
+            font-family: "poppins";
         }
     </style>
     <!--//END ABOUT IMAGE -->
@@ -85,7 +85,7 @@
                                 Rencana kerja disusun sebagai langkah persiapan. Langkah pertama yang dilakukan adalah membahas pembukaan sekolah SMP dan SMA yang direncanakan mulai menerima siswa baru pada tahun pelajaran 1991/1992.
                             </p>
                             <p align="justify" class="lh-base">
-                                Kemudian pada tahun pelajaran 1991/1992 telah menerima 26 siswa SMA yang dititipkan pada SMAN II Tanjungkarang, dengan nama sekolah <i> <b>Perguruan Nurul Ulum (cahaya utama) </b> </i>. Nama ini dicetuskan oleh Bapak Syamsuddin Tohir (Kakanwil Agama Propinsi Lampung), orang yang paling bersemangat dalam perintisan pembangunan sekolah umum (plus).
+                                Kemudian pada tahun pelajaran 1991/1992 telah menerima 26 siswa SMA yang dititipkan pada SMAN II Tanjungkarang, dengan nama sekolah <i> <b>Perguruan Nurul Ulum (cahaya utama)</b>.</i> Nama ini dicetuskan oleh Bapak Syamsuddin Tohir (Kakanwil Agama Propinsi Lampung), orang yang paling bersemangat dalam perintisan pembangunan sekolah umum (plus).
                             </p>
                             <p align="justify" class="lh-base">
                                 Pada bulan Agustus 1991, ketika Bapak Syamsuddin Tohir dan Bapak Harris Hasyim berkonsultasi dengan Ibu Sri Mulyati Poedjono di kediaman Gubernur, Ibu Sri Mulyati Poedjono meminta disebutkan beberapa alternatif nama, yang akhirnya dipilih nama <b>Al Kautsar</b>. Alasannya karena mencerminkan visi perjuangan ummat dan populer serta layak jual (<i>marketable</i>). Akhirnya nama Nurul Ulum diganti dengan Al Kautsar.
@@ -97,9 +97,9 @@
                                 Atas inisiatif Gubernur Poedjono Pranyoto, dalam suatu pengajian rutin, pembangunan Kampus Al Kautsar perlu segera dimulai. Kesepakatan yang ditetapkan adalah:
                             </p>
                             <ul type="disc">
-                                <li>Kelompok pengajian Al Amal membuat mandat kepada Ibu Sri Mulyati Poedjono Pranyoto, untuk membentuk dan memimpin Yayasan AL Kautsar</li>
+                                <li>Kelompok pengajian Al Amal membuat mandat kepada Ibu Sri Mulyati Poedjono Pranyoto, untuk membentuk dan memimpin Yayasan AL Kautsar.</li>
                                 <li>Meminta kesediaan seluruh unsur dan tokoh agama Islam untuk mulai mempromosikan berdirinya Perguruan Al Kautsar dalam berbagai kegiatan dakwah dan mimbar Jumat.</li>
-                                <li>Meminta dukungan seluruh aparatur pemerintah yang beragama Islam unrtuk bergortong royong mendukung pembangunannya.</li>
+                                <li>Meminta dukungan seluruh aparatur pemerintah yang beragama Islam unrtuk bergotong royong mendukung pembangunannya.</li>
                                 <li>Meminta dukungan Pemda Kabupaten dan Kotamadya untuk membantu mempercepat pendidirian kampus.</li>
                                 <li>Mengusulkan kepada Pemerintah Daerah Tingkat I Lampung untuk mendukung pembangunan kampus secara bertahap melalui dukungan APBD.</li>
                                 <li>Menugaskan kepada Ibu Sri Mulyati Poedjono untuk menghimpun dana masyarakat dan swasta bagi pembangunan kampus.</li>
@@ -111,7 +111,7 @@
                             <p align="justify" class="lh-base">Pada tanggal 16 November 1991, berdasarkan keputusan pengajian Al Amal di rumah Bapak A. Mukti, Kajati Propinsi Lampung pada waktu itu, ditetapkan pendirian Yayasan Al Kautsar dengan surat <b>Pernyataan Kesepakatan/Mandat Nomor: Khusus/Al Amal/I/1991 tanggal 16 Nopember 1991 </b> susunan sebagai berikut :
                             <ul type="disc">
                                 <li>Poedjono Pranyoto, Gubernur Prop. Lampung sebagai Pelindung;</li>
-                                <li>Ibu Sri Mulyati Poedjono Pranyoto, Isteri Gubernur sebagai Ketua;</li>
+                                <li>Ibu Sri Mulyati Poedjono Pranyoto, Istri Gubernur sebagai Ketua;</li>
                                 <li>Man Hasan, Wakil Gubernur Prop. Lampung sebagai Anggota;</li>
                                 <li>Fauzi Saleh, Sekwilda Prop. Lampung sebagai Anggota;</li>
                                 <li>Alhusniduki Hamim, SE., M.Sc., Rektor UNILA sebagai Anggota;</li>
@@ -125,8 +125,8 @@
 
                             <h4><b>Pembangunan Kampus Al Kautsar</b></h4>
                             <ul type="disc">
-                                <li>Peletakan batu pertama pembangunan Kampus Perguruan Al Kautsar dilakukan oleh Gubernur Propinsi Lampung, Poedjono Pranyoto dengan suatu upacara sederhana yang hidmat pada tanggal 20 Agustus 1992. Ibu Sri Mulyati Poedjono dibawah siraman hujan menanam beringin putih (depan gedung SMP)</li>
-                                <li>Program pembangunan fisik yang pertama ditetapkan dengan dana Pemda dengan dukungan DPRD Propinsi Tingkat I Lampung yang dipimpin oleh Bp. Sundoro Brotoatmodjo dan dilanjutkan kemudian dengan dukungan penuh dari DPRD Propinsi Tk.I Lampung yang dipimpin oleh Bp. Kariyotomo.</li>
+                                <li>Peletakan batu pertama pembangunan Kampus Perguruan Al Kautsar dilakukan oleh Gubernur Propinsi Lampung, Poedjono Pranyoto dengan suatu upacara sederhana yang khidmat pada tanggal 20 Agustus 1992. Ibu Sri Mulyati Poedjono dibawah siraman hujan menanam beringin putih (depan gedung SMP)</li>
+                                <li>Program pembangunan fisik yang pertama ditetapkan dengan dana Pemda dengan dukungan DPRD Propinsi Tingkat I Lampung yang dipimpin oleh Bapak Sundoro Brotoatmodjo dan dilanjutkan kemudian dengan dukungan penuh dari DPRD Propinsi Tk.I Lampung yang dipimpin oleh Bapak Kariyotomo.</li>
                                 <li>Proses pembangunan lantai II dan III dan selanjutnya sangat unik dan heroik. Pada suatu pengajian Al Amal bulan Januari dan Februari 1993, Bapak. Drs. Syamsuddin Tohir menghimbau agar semua unsur pengajian memberikan perhatian dan menyumbangkan pemikiran tenaga dan dana untuk membangun kampus ini. Beliau sendiri mengusulkan agar 3 bulan tunjangan struktural seluruh pegawai Kanwil Agama menginfakkannya untuk pembangunan Kampus Al Kautsar.</li>
                                 <li>Hampir seluruh Kepala Dinas dan Instansi Propinsi menyumbang rata-rata Rp.15-25 juta dan para Bupati menyumbangkan dananya melalui APBD sebesar Rp.25-30 juta.</li>
                                 <li>Walikota Kotamadya Bandar Lampung, Bapak Drs. Suharto (pada periode waktu itu, Drs. Muswardi Thaher duduk sebagai anggota DPRD Kotamadya Bandar Lampung periode 1988-1999), menyumbangkan pembangunan jembatan penyeberangan dari Kampus Al Kautsar ke kompleks Islamic Centre senilai Rp.295 juta melalui APBD Kotamadya.</li>
@@ -134,7 +134,7 @@
                             <br>
 
                             <h4><b>Dukungan Swasta dan Masyarakat</b></h4>
-                            <p align="justify" class="lh-base">Berkat promosi yang dilakukan oleh Gubernur Bapak Poedjono Pranyoto kepada kalangan swasta di Lampung dan Jakarta, sumbangan yang cukup besar diterima oleh Yayasan Al Kautsar dari PT. Salim Grup Jakarta, PT. Dipasena Citra Darmaja (DCD), PT. Great Giant Pine Apple (GGPC) , PT. Citra Pertiwi Bratasena (CPB), dan oleh Ibu Sri Mulyati Poedjono sendiri yang membangun TK Al Kautsar atas nama pribadinya, serta PT.Tiga Satu (31) yang dipimpin oleh Bp. Faisol Djausal menyumbangkan dana pembangunan melengkapi pembangunan gedung SD.</p>
+                            <p align="justify" class="lh-base">Berkat promosi yang dilakukan oleh Gubernur Bapak Poedjono Pranyoto kepada kalangan swasta di Lampung dan Jakarta, sumbangan yang cukup besar diterima oleh Yayasan Al Kautsar dari PT. Salim Grup Jakarta, PT. Dipasena Citra Darmaja (DCD), PT. Great Giant Pine Apple (GGPC) , PT. Citra Pertiwi Bratasena (CPB), dan oleh Ibu Sri Mulyati Poedjono sendiri yang membangun TK Al Kautsar atas nama pribadinya, serta PT.Tiga Satu (31) yang dipimpin oleh Bapak Faisol Djausal menyumbangkan dana pembangunan melengkapi pembangunan gedung SD.</p>
                             </p>
                             <br>
 
@@ -142,14 +142,14 @@
                             <p align="justify" class="lh-base">
                                 SMA S Al Kautsar sebagai salah satu sekolah di bawah Naungan Yayasan Al Kautsar sejak didirikannya bercita-cita menjadi salah satu sekolah terbaik di Lampung bahkan di Indonesia. Semangat itu terus menyala sejak dibangun dan diresmikan oleh Menteri Pendidikan dan Kebudayaan RI Bapak Prof. Dr. Ing. Wardiman Djojonegoro. Akhirnya dalam kurun waktu 5 tahun SMA Al Kautsar telah menjadi sekolah dambaan dan idaman masyarakat, hal itu terbukti dengan besarnya animo masyarakat untuk menyekolahkan anak-anaknya di SMA S Al Kautsar. Secara mutu juga SMA S Al Kautsar pernah menduduki peringkat 5 jurusan IPS dan peringkat 6 untuk IPA di Propinsi Lampung.
                             </p>
-                            <p align="justify" class="lh-base"> SMA S Al Kautsar pertama kali menerima murid pada tahun 1993/1994. Sebagai sekolah yang baru berdiri dan belum dikenal oleh masyarakat menyebabkan adanya kekhawatiran dari pengurus Yayasan tentang ada tidaknya minat masyarakat menyekolahkan anaknya di SMA Al kautsar, sehingga pada tahun itu kebijakannya adalah 10 calon murid pendaftar pertama langsung diterima. Berkat penyebaran informasi secara langsung ke sekolah-sekolah seperti Kalianda, Metro/lampung Tengah, Kotabumi dan Pringsewu maka pada tahun pertama mendapatkan siswa sebanyak kurang lebih 210 orang, yang pada waktu itu masih menyatu dengan SMP Al Kautsar baik gedung maupun guru dan karyawannya, yang unik pada tahun itu adalah hingga penataran P4 selesai sekolah belum memiliki Kepala Sekolah
+                            <p align="justify" class="lh-base"> SMA S Al Kautsar pertama kali menerima murid pada tahun 1993/1994. Sebagai sekolah yang baru berdiri dan belum dikenal oleh masyarakat menyebabkan adanya kekhawatiran dari pengurus Yayasan tentang ada tidaknya minat masyarakat menyekolahkan anaknya di SMA Al Kautsar, sehingga pada tahun itu kebijakannya adalah 10 calon murid pendaftar pertama langsung diterima. Berkat penyebaran informasi secara langsung ke sekolah-sekolah seperti Kalianda, Metro/Lampung Tengah, Kotabumi dan Pringsewu maka pada tahun pertama mendapatkan siswa sebanyak kurang lebih 210 orang, yang pada waktu itu masih menyatu dengan SMP Al Kautsar baik gedung maupun guru dan karyawannya, yang unik pada tahun itu adalah hingga penataran P4 selesai sekolah belum memiliki Kepala Sekolah.
                             </p>
                             <p align="justify" class="lh-base">Tahun 1994-1996 sekolah sering mendapat kunjungan para menteri seperti Menteri Pendidikan dan Kebudayaan RI, Menteri Riset dan Teknologi RI, Menteri Agama RI dan pejabat-pejabat pusat lainnya. Prestasi demi prestasi diraih oleh SMA S Al Kautsar baik pada bidang akademis maupun non akademis di Bandar Lampung maupun Lampung bahkan pada event nasional.
                             </p>
                             <p align="justify" class="lh-base">
-                                Memasuki tahun 2002/2003 SMA S Al Kautsar memasuki babak baru dengan dibukanya kelas unggul akademis. Dari kelas unggul inilah akhirnya perkembangan sekolah mengalami kemajuan yang cukup pesat, bahkan bisa dikatakan sejajar dengan sekolah-sekolah vafourit di Lampung seperti SMAN 2 dan SMA Xaverius pada beberapa bidang. Dilihat dari prosentase kelulusan dan siswa yang diterima di PTN atau PTS vafouritpun dari tahun ke tahun mengalami perkembangan yang sangat tajam. Sukses dalam pengelolaan kelas unggul, maka pada tahun pelajaran 2004/2005 SMA Al-Kautsar membuka kelas Plus.
+                                Memasuki tahun 2002/2003 SMA S Al Kautsar memasuki babak baru dengan dibukanya kelas unggul akademis. Dari kelas unggul inilah akhirnya perkembangan sekolah mengalami kemajuan yang cukup pesat, bahkan bisa dikatakan sejajar dengan sekolah-sekolah favorit di Lampung seperti SMAN 2 dan SMA Xaverius pada beberapa bidang. Dilihat dari persentase kelulusan dan siswa yang diterima di PTN atau PTS favorit pun dari tahun ke tahun mengalami perkembangan yang sangat tajam. Sukses dalam pengelolaan kelas unggul, maka pada tahun pelajaran 2004/2005 SMA Al-Kautsar membuka kelas Plus.
                                 <br>
-                                Saat ini SMA S Al Kautsar telah meluluskan siswa kurang lebih 8000 siswa dam kepala SMA S Al Kautsar dari periode ke periode sebagai berikut:
+                                Saat ini SMA S Al Kautsar telah meluluskan siswa kurang lebih 8000 siswa dan Kepala SMA S Al Kautsar dari periode ke periode sebagai berikut:
                             <ul type="disc">
                                 <li>Drs. Zainal (periode 1993 - 1995)</li>
                                 <li>Drs. Abdul Sani Djuned (periode 1995 -1996)</li>
@@ -157,7 +157,7 @@
                                 <li>Drs. H. Sunardi (periode 2005 - 2012)</li>
                                 <li>Drs. H. Joko Santoso (periode 2012 -2015)</li>
                                 <li>Drs. H. Sukijo, M.Pd. (periode 2015 - 2015) </li>
-                                <li>H. Eko Anzair, M.Si. (periode 2015 s.d Sekarang)</li>
+                                <li>H. Eko Anzair, M.Si. (periode 2015 s.d sekarang)</li>
                             </ul>
                             </p>
                             <p align="justify" class="lh-base">
--- application/views/depan/v_visi.php
+++ application/views/depan/v_visi.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Visi & Misi</title>
     <link href="assets/aset/img/favicon.png" rel="icon">
-  <link href="assets/aset/img/apple-touch-icon.png" rel="apple-touch-icon">
+    <link href="assets/aset/img/apple-touch-icon.png" rel="apple-touch-icon">
 
     <link rel="shorcut icon" href="theme/images/icon.png">
     <!-- Bootstrap CSS -->
@@ -23,21 +23,21 @@
     <link rel="stylesheet" href="theme/css/slick-theme.css">
     <link rel="stylesheet" href="theme/css/owl.carousel.min.css">
 
-     <!-- Google Fonts -->
- <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
-<!-- Vendor CSS Files -->
-<link href="assets/aset/vendor/animate.css/animate.min.css" rel="stylesheet">
-<link href="assets/aset/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-<link href="assets/aset/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-<link href="assets/aset/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-<link href="assets/aset/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-<link href="assets/aset/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+    <!-- Vendor CSS Files -->
+    <link href="assets/aset/vendor/animate.css/animate.min.css" rel="stylesheet">
+    <link href="assets/aset/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/aset/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/aset/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/aset/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/aset/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
 
-<!-- Template Main CSS File -->
-<link href="assets/aset/css/style.css" rel="stylesheet">
+    <!-- Template Main CSS File -->
+    <link href="assets/aset/css/style.css" rel="stylesheet">
     <!-- Main CSS -->
-    
+
     <?php
     function limit_words($string, $word_limit)
     {
@@ -51,8 +51,8 @@
 
 <body>
     <style>
-        .container{
-            font-family:"poppins";
+        .container {
+            font-family: "poppins";
         }
     </style>
     <!--//END ABOUT IMAGE -->
@@ -64,56 +64,56 @@
                     <h2 align="center">Visi SMA Al Kautsar</h2>
                     <p align="center">Al-Kautsar Unggul, Islami, dan Global</p><br><br>
 
-                    
-                    <h2 align="center">Misi SMA Al Kautsar</h2>   
-                    <ol align="justify">             
-                    <li>Menjadikan sekolah berprestasi di bidang akademik dan nonakademik di tingkat nasional</li>
-                    <li>Meningkatkan kompetensi pendidik dan tenaga kependidikan</li>
-                    <li>Mewujudkan sarana dan prasarana sekolah yang lengkap, modern dan berwawasan lingkungan</li>
-                    <li>Mewujudkan pendidikan yang berkarakter dengan mengintegrasikan nilai keislaman ke dalam proses pembelajaran.</li>
-                    <li>Membentuk peserta didik yang berkarakter dan berjiwa kepemimpinan islami</li>
-                    <li>Mewujudkan sistem pengelolaan sekolah dengan berbasis teknologi, informasi dan komunikasi</li>
-                    <li>Meningkatkan kemampuan literasi warga sekolah</li>
-                    <li>Mewujudkan lulusan berwawasan global serta mampu melanjutkan pendidikan di dalam dan luar negeri</li>
-                    <li>Menjalin kerja sama dengan orang tua, masyarakat, pemerintah dan stakeholder untuk peningkatan mutu pendidikan.</li><br>
+
+                    <h2 align="center">Misi SMA Al Kautsar</h2>
+                    <ol align="justify">
+                        <li>Menjadikan sekolah berprestasi di bidang akademik dan nonakademik di tingkat nasional</li>
+                        <li>Meningkatkan kompetensi pendidik dan tenaga kependidikan</li>
+                        <li>Mewujudkan sarana dan prasarana sekolah yang lengkap, modern dan berwawasan lingkungan</li>
+                        <li>Mewujudkan pendidikan yang berkarakter dengan mengintegrasikan nilai keislaman ke dalam proses pembelajaran</li>
+                        <li>Membentuk peserta didik yang berkarakter dan berjiwa kepemimpinan islami</li>
+                        <li>Mewujudkan sistem pengelolaan sekolah dengan berbasis teknologi, informasi dan komunikasi</li>
+                        <li>Meningkatkan kemampuan literasi warga sekolah</li>
+                        <li>Mewujudkan lulusan berwawasan global serta mampu melanjutkan pendidikan di dalam dan luar negeri</li>
+                        <li>Menjalin kerja sama dengan orang tua, masyarakat, pemerintah dan stakeholder untuk peningkatan mutu pendidikan.</li><br>
                     </ol>
                     <br>
 
-                    
+
                     <h2 align="center">Tujuan SMA AL Kautsar</b></h4>
-                    
-                    <b>Tujuan Pendidikan Nasional</b>
+
+                        <b>Tujuan Pendidikan Nasional</b>
                         <p align="justify">Tujuan pendidikan nasional tercantum dalam Undang-Undang Nomor 20 Tahun 2003 tentang Sistem Pendidikan Nasional, pasal 3, “ mengembangkan potensi peserta didik agar menjadi manusia yang beriman dan bertagwa kepada Tuhan Yang Maha Esa, berakhlak mulia, sehat, berilmu, cakap, kreatif, mandiri, dan menjadi warga negara yang demokratis serta bertanggung jawab”.</p><br>
-                    <b>Tujuan Pendidikan Sekolah Menengah Atas</b>
+                        <b>Tujuan Pendidikan Sekolah Menengah Atas</b>
                         <p align="justify">Tujuan pendidikan menengah adalah meningkatkan kecerdasan, pengetahuan, kepribadian, akhlak mulia, serta keterampilan untuk hidup mandiri dan mengikuti pendidikan lebih lanjut.</p><br>
-                    <b>Tujuan SMA S Al Kautsar adalah sebagai berikut:</b>
-                    <ol align="justify">
-                    <li>Diraihnya juara olimpiade sains, FLS2N, LDBI, prestasi akademik dan nonakademik lainnya di tingkat kota, provinsi, nasional, dan internasional.</li>
-                    <li>Memiliki tenaga pendidik berkualifikasi S2 sebanyak 60 % tahun 2025.</li>
-                    <li>Memiliki tenaga kependidikan berkualifikasi S1, pustakawan dan laboran yang tersertifikasi.</li>
-                    <li>Membiasakan membaca Al Quran 1 juz dalam 10 hari semua warga sekolah.</li>
-                    <li>Tersedia dan teroptimalkannya laboratorium dan perpustakaan elektronik (e-library) sebagai sumber belajar di sekolah.</li>
-                    <li>Tersedianya sarana dan prasarana sekolah yang lengkap, modern dan berwawasan lingkungan tahun 2025.</li>
-                    <li>Terintegrasikannya nilai-nilai Islam ke dalam proses pendidikan dan pembelajaran di lingkungan sekolah.</li>
-                    <li>Terwujudnya lulusan yang mencintai Al Quran dan mampu membaca dengan baik dan benar serta hafal minimal 9 surat pilihan.</li>
-                    <li>Terwujudnya SMA S Al Kautsar sebagai sekolah tujuan utama bagi orang tua untuk menyekolahkan putra-putrinya khususnya yang berdomisili di provinsi Lampung dan seluruh Indonesia umumnya.</li>
-                    <li>Menghasilkan peserta didik berakhlakhul karimah serta memiliki kemampuan menyampaikan kultum.</li>
-                    <li>Terselenggaranya kegiatan leadership di semua peserta didik.</li>
-                    <li>Terselenggaranya pembelajaran yang berbasis penguasaan teknologi informasi (IT).</li>
-                    <li>Terselenggaranya sistem penilaian berbasis IT, terciptanya Sistem Informasi Manajemen sekolah yang berbasis IT dan terwujudnya informasi sekolah berbasis WEB, IG dan Facebook .</li>
-                    <li>Mewujudkan peserta didik untuk membaca buku fiksi nonfiksi setiap 15 menit sebelum pembelajaran dimulai.</li>
-                    <li>Tersedianya buku-buku non pelajaran yang cukup bervariasi untuk kegiatan literasi.</li>
-                    <li>Tersedianya pojok baca di dalam dan di luar kelas.</li>
-                    <li>Tersedianya akses internet yang memadai untuk literasi digital.</li>
-                    <li>Diterimanya lulusan di perguruan tinggi negeri dan kedinasan baik di Indonesia dan Perguruan Tinggi di Luar negeri setiap tahunnya lebih dari sama dengan 80%.</li>
-                    <li>Terjalinnya kerjasama antara sekolah dengan Organisasi Ikatan Alumni SMA Al Kautsar dalam berbagai bentuk Kegiatan.</li>
-                    <li>Terselenggaranya kegiatan parenting secara rutin setiap akhir tahun pelajaran.</li>
-                    </ol>
-                    
+                        <b>Tujuan SMA S Al Kautsar adalah sebagai berikut:</b>
+                        <ol align="justify">
+                            <li>Diraihnya juara olimpiade sains, FLS2N, LDBI, prestasi akademik dan nonakademik lainnya di tingkat kota, provinsi, nasional, dan internasional.</li>
+                            <li>Memiliki tenaga pendidik berkualifikasi S2 sebanyak 60 % tahun 2025.</li>
+                            <li>Memiliki tenaga kependidikan berkualifikasi S1, pustakawan dan laboran yang tersertifikasi.</li>
+                            <li>Membiasakan membaca Al Quran 1 juz dalam 10 hari semua warga sekolah.</li>
+                            <li>Tersedia dan teroptimalkannya laboratorium dan perpustakaan elektronik (e-library) sebagai sumber belajar di sekolah.</li>
+                            <li>Tersedianya sarana dan prasarana sekolah yang lengkap, modern dan berwawasan lingkungan tahun 2025.</li>
+                            <li>Terintegrasikannya nilai-nilai Islam ke dalam proses pendidikan dan pembelajaran di lingkungan sekolah.</li>
+                            <li>Terwujudnya lulusan yang mencintai Al Quran dan mampu membaca dengan baik dan benar serta hafal minimal 9 surat pilihan.</li>
+                            <li>Terwujudnya SMA S Al Kautsar sebagai sekolah tujuan utama bagi orang tua untuk menyekolahkan putra-putrinya khususnya yang berdomisili di provinsi Lampung dan seluruh Indonesia umumnya.</li>
+                            <li>Menghasilkan peserta didik berakhlakhul karimah serta memiliki kemampuan menyampaikan kultum.</li>
+                            <li>Terselenggaranya kegiatan leadership di semua peserta didik.</li>
+                            <li>Terselenggaranya pembelajaran yang berbasis penguasaan teknologi informasi (IT).</li>
+                            <li>Terselenggaranya sistem penilaian berbasis IT, terciptanya Sistem Informasi Manajemen sekolah yang berbasis IT dan terwujudnya informasi sekolah berbasis WEB, IG dan Facebook .</li>
+                            <li>Mewujudkan peserta didik untuk membaca buku fiksi nonfiksi setiap 15 menit sebelum pembelajaran dimulai.</li>
+                            <li>Tersedianya buku-buku non pelajaran yang cukup bervariasi untuk kegiatan literasi.</li>
+                            <li>Tersedianya pojok baca di dalam dan di luar kelas.</li>
+                            <li>Tersedianya akses internet yang memadai untuk literasi digital.</li>
+                            <li>Diterimanya lulusan di perguruan tinggi negeri dan kedinasan baik di Indonesia dan Perguruan Tinggi di Luar negeri setiap tahunnya lebih dari sama dengan 80%.</li>
+                            <li>Terjalinnya kerjasama antara sekolah dengan Organisasi Ikatan Alumni SMA Al Kautsar dalam berbagai bentuk kegiatan.</li>
+                            <li>Terselenggaranya kegiatan parenting secara rutin setiap akhir tahun pelajaran.</li>
+                        </ol>
 
 
 
-                    <!-- <div>
+
+                        <!-- <div>
                         <img src="?php echo base_url() . 'document/Visi Misi SMA S Al Kautsar_001.png" alt="#">
                         <img src="?php echo base_url() . 'document/Visi Misi SMA S Al Kautsar_002.png" alt="#">
                         <img src="?php echo base_url() . 'document/Visi Misi SMA S Al Kautsar_003.png" alt="#">
